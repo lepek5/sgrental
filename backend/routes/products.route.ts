89c950628e724d347fc263442f0985a32577b9bb
lepek5 <jari.mikkonen@gmail.com>
2023-04-07 14:39:37 +0300

    Dev: is possible in containers

--- backend/routes/products.route.ts
+++ backend/routes/products.route.ts
@@ -2,5 +2,4 @@ import express from "express";
 import ProductController from "../controllers/product.controller";
 const ProductsRouter = express.Router();
 ProductsRouter.get("/", ProductController.getAll);
-ProductsRouter.get("/get", ProductController.get);
 export default ProductsRouter;
--- front/dev.Dockerfile
+++ front/dev.Dockerfile
@@ -1,7 +1,7 @@
 FROM node:alpine
-WORKDIR /usr/frontend
-COPY ./front/package*.json ./
+WORKDIR /usr/src/app
+COPY ./package*.json ./
 RUN npm install 
-COPY ./front/ ./
+COPY . ./
 EXPOSE 4000
 CMD npm run dev
