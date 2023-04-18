0f232d05a21adc2713cc91f83ee59f41ce06d1c8
lepek5 <jari.mikkonen@gmail.com>
2023-04-18 20:12:19 +0300

    Frontend: Usercontext created

--- front/src/utils/use-auth.tsx
+++ front/src/utils/use-auth.tsx
@@ -1,34 +1,16 @@
-import React, { useState, useEffect, useContext, createContext, ReactElement } from "react";
-import { IUserToken } from "../interfaces/IUser";
-interface IAuthContext {
-  id: number,
-  email: string,
-  token: string
-}
-const authContext = createContext<IUserToken>({
-  id: null,
-  email: null,
-  token: null
-});
+import React, { createContext, useContext } from "react";
+import { useQuery } from "react-query";
+import userService from "../services/user.service";
 
-export function ProvideAuth({ children }: {children : any}) {
-  const auth = useProvideAuth();
-  return (<authContext.Provider value={auth}>{children}</authContext.Provider>);
+export const UserContext = createContext(null);
+const Auth = ({ children }: { children: React.ReactNode[] | React.ReactNode }) => {
+  const { data: user } = useQuery("user", async () => {
+    return await userService.whoami();
+  })
+  return (
+    <UserContext.Provider value={user}>
+      {children}
+    </UserContext.Provider>
+  )
 }
-// Hook for child components to get the auth object ...
-// ... and re-render when it changes.
-export const useAuth = () => useContext(authContext);
-
-// Provider hook that creates auth object and handles state
-function useProvideAuth() {
-  const [user, setUser] = useState<IUserToken>({
-    id: 0,
-    email: "",
-    token: ""
-  });
-  return {
-    id: user.id,
-    email: user.email,
-    token: user.token
-  };
-}
+export default Auth;
