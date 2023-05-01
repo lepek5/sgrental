5cc8e6a1c7a58632588f4918d876180ea7be5d48
lepek5 <jari.mikkonen@gmail.com>
2023-05-01 20:36:19 +0300

    Refactor/Cleaning repo

--- backend/middleware/cookieHandler.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-const cookieHandler = async (req: Request, res: Response, next: NextFunction) => {
-  req.cookies = req.headers.cookie;
-  next();
-}
-export default cookieHandler;
