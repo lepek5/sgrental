ad1361a567ad9185ac2ff01396cad725de9380cd
lepek5 <jari.mikkonen@gmail.com>
2023-05-01 20:32:26 +0300

    Refactor

--- backend/services/product.service.ts
+++ backend/services/product.service.ts
@@ -1,66 +1,69 @@
 import categoryService from "./category.service";
 import { Product, Category, ProductDetail } from "../models";
 import { IProduct } from "../interfaces/IProduct";
-const ProductService = {
-  getAll: async (): Promise<IProduct[] | any> => {
-    try {
-      const result = await Product.findAll({
-        nest: true,
-        include: [{
-          model: Category,
-          attributes: {
-            exclude: ["id"]
-          },
-          through: {attributes: []}
-        }],
-      });
-      return result;
-    } catch (error: unknown) {
-      throw error;
-    }
-  },
-  getById: async (id: string): Promise<any> => {
-    try {
-      const result = await Product.findByPk(id, {
-        nest: true,
-        include: [{
-          model: Category,
-          attributes: {
-            exclude: ["id"]
-          },
-          through: {
-            attributes: []
-          }
-        }]
-      });
-      return result;
-    } catch (error: unknown) {
-      return error;
-    }
-  },
-  createProduct: async (payload: IProduct): Promise<any> => {
-    const { categories, ...product } = payload;
-    try {
-      const result = await Product.create(product);
-      if (result) {
-        const { id: productId } = result.toJSON();
-        categories?.forEach(async (value: any) => {
-          try {
-            const cat = await categoryService.createCategory(value);
-            const { id: categoryId } = cat;
-            const result = await ProductDetail.create({
-              productId, categoryId
-            });
-            if (!result) throw new Error("Error pairing product and category");
-          } catch (err) {
-            throw err;
-          }
-        })
-      }
-      return result.toJSON();
-    } catch (err) {
-      throw err;
-    }
+
+const getAll = async (): Promise<IProduct[] | any> => {
+  try {
+    const result = await Product.findAll({
+      nest: true,
+      include: [{
+        model: Category,
+        attributes: {
+          exclude: ["id"]
+        },
+        through: {attributes: []}
+      }],
+    });
+    return result;
+  } catch (error: unknown) {
+    throw error;
   }
 };
-export default ProductService;
+const getById = async (id: string): Promise<any> => {
+  try {
+    const result = await Product.findByPk(id, {
+      nest: true,
+      include: [{
+        model: Category,
+        attributes: {
+          exclude: ["id"]
+        },
+        through: {
+          attributes: []
+        }
+      }]
+    });
+    return result;
+  } catch (error: unknown) {
+    return error;
+  }
+};
+const createProduct = async (payload: IProduct): Promise<any> => {
+  const { categories, ...product } = payload;
+  try {
+    const result = await Product.create(product);
+    if (result) {
+      const { id: productId } = result.toJSON();
+      categories?.forEach(async (value: any) => {
+        try {
+          const cat = await categoryService.createCategory(value);
+          const { id: categoryId } = cat;
+          const result = await ProductDetail.create({
+            productId, categoryId
+          });
+          if (!result) throw new Error("Error pairing product and category");
+        } catch (err) {
+          throw err;
+        }
+      })
+    }
+    return result.toJSON();
+  } catch (err) {
+    throw err;
+  }
+}
+export default {
+  getAll,
+  getById,
+  createProduct
+};
--- backend/services/user.service.ts
+++ backend/services/user.service.ts
@@ -1,70 +1,72 @@
 import config from "../config";
-import Database from "../database";
 import User from "../models/user";
 import { HtmlError } from "../utils/customErrors";
 import { httpStatus } from "../utils/httpStatus";
-import sql from "../utils/sql";
-const TABLE = "users";
-const userService = {
-  login: async (email: string, password: string) => {
-    try {
-      const user = await User.findOne({
-        where: {
-          email: email
-        }
-      });
-      if (!user) {
-        throw new HtmlError(httpStatus.NOT_FOUND, "User not found");
+
+const login = async (email: string, password: string) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: email
       }
-      const login = await config.utils.comparePassword(password, user.toJSON().password);
-      if (!login) {
-        throw new HtmlError(httpStatus.UNAUTHORIZED, "Invalid credentials on login")
-      }
-      return user;
-    } catch (err) {
-      throw err;
+    });
+    if (!user) {
+      throw new HtmlError(httpStatus.NOT_FOUND, "User not found");
     }
-  },
-  createUser: async (payload: any) => {
-    try {
-      const result = await User.create(payload);
-      console.log("create user", result)
-      return result.toJSON();
-    } catch (err) {
-      throw err;
+    const login = await config.utils.comparePassword(password, user.toJSON().password);
+    if (!login) {
+      throw new HtmlError(httpStatus.UNAUTHORIZED, "Invalid credentials on login")
     }
-  },
-  getAll: async () => {
-    try {
-      const result = await User.findAll({
-        attributes: {
-          exclude: ["password"]
-        }
-      });
-      return result;
-    } catch (err) {
-      throw err;
-    }
-  },
-  getByEmail: async (email: string) => {
-    try {
-      const result = await User.findOne({
-        where: {
-          email: email
-        }
-      });
-      return result;
-    } catch (err) {
-      throw err;
-    }
-  },
-  getById: async (id: string) => {
-    try {
-      const result = await User.findByPk(id);
-      return result;
-    } catch (err) {
-      throw err;
-    }
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+const createUser = async (payload: any) => {
+  try {
+    const result = await User.create(payload);
+    console.log("create user", result)
+    return result.toJSON();
+  } catch (err) {
+    throw err;
   }
 };
-export default userService;
+const getAll = async () => {
+  try {
+    const result = await User.findAll({
+      attributes: {
+        exclude: ["password"]
+      }
+    });
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+const getByEmail = async (email: string) => {
+  try {
+    const result = await User.findOne({
+      where: {
+        email: email
+      }
+    });
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+const getById = async (id: string) => {
+  try {
+    const result = await User.findByPk(id);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+export default {
+  login,
+  getAll,
+  getByEmail,
+  getById,
+  createUser
+};
