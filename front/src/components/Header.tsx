e5790c51b670273e35f7448998c658285cc329ed
lepek5 <jari.mikkonen@gmail.com>
2023-05-03 12:27:50 +0300

    Frontend: added links to nav for home- and contact-pages

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { useQuery } from "react-query";
-import userService from "../services/user.service";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <h1>Urheiluvälinevuokraamo Oy</h1>
+      <div className="div" style={{display: "flex", gap: "0.9rem"}}>
+        <Link className="main-nav" to="/">Etusivu</Link>
+        <Link className="main-nav" to="/contact">Ota yhteyttä</Link>
+      </div>
       <Navbar />
     </header>
   )
